cdc9bc3e8680c4e3ea59018cdce6962a3cc276f6
Minh Ha <nhat.minh.ha@gmail.com>
2012-09-05 03:32:57 +1200

    bundle install --skip-test-unit, rspec-rails, capybara, pg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleAppMhartlKatze::Application.config.secret_token = 'd280e0f880879ac279423d4a05ed207d713086dc4a5b9998d6b9dcdd22278d30ee5d6ed4dc71f4daaa67d9525d8865773a64feb6b61fa8a96d7b3343dfb6fddf'
