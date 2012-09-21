2eefe88a0af0f7277ee69bffd7438b082d8686c9
Minh Ha <nhat.minh.ha@gmail.com>
2012-09-21 15:20:27 +1200

    dynamic Application Helper for title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
 	# http://ruby.railstutorial.org/chapters/rails-flavored-ruby#top 4.2
 	# Returns full title and | depending on the value provided
+	
 	def full_title(page_title)
 		base_title = "Ruby on Rails Tutorial Sample App"
 		if page_title.empty?
