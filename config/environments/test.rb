471cd86569ba770be166cb941a00c6a6f5ff3b80
Minh Ha <nhat.minh.ha@gmail.com>
2012-12-11 01:15:07 +1300

    reduce BCrypt cost factor in order to speed up test

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,4 +34,10 @@ SimpleAppMhartlKatze::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+
+  # Speed up tests by lowering BCrypt's cost function.
+  require 'bcyrpt'
+  silence_warnings do
+    BCrypt::Engine::DEFAULT_COST = BCrypt::Engine::MIN_COST
+  end
 end
