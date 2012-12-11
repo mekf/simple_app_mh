fa3cb9e38cf30ec31dc1c89d58b4cb3e010be50e
Minh Ha <nhat.minh.ha@gmail.com>
2012-12-12 04:22:19 +1300

    using FactoryGirl

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,7 +23,7 @@ class User < ActiveRecord::Base
 
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, presence: true, uniqueness: { case_sensitive: false},
+  validates :email, presence: true, uniqueness: { case_sensitive: false },
                     format: { with: VALID_EMAIL_REGEX }
 
   validates :password, presence: true, length: { minimum: 6 }
