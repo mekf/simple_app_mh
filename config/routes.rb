fc18a5aeef15f066a9e475c3e3da092a937b420d
Minh Ha <nhat.minh.ha@gmail.com>
2013-01-01 13:36:50 +1300

    partial for following, unfollowing buttons

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ SimpleAppMhartlKatze::Application.routes.draw do
   
   resources :sessions, only: [:new, :create, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 
   #troll
   get 'troll', to: 'users#troll'
